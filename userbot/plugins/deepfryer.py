88d6e278ad611b1c6c1439c83f954fff9728b68b
BufaloBob <64494044+BufaloBob@users.noreply.github.com>
2020-05-02 11:35:39 +0200

    Update deepfryer.py

--- userbot/plugins/deepfryer.py
+++ userbot/plugins/deepfryer.py
@@ -27,7 +27,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-""" Userbot module for frying stuff. ported by @NeoMatrix90 """
+""" Userbot module for frying stuff. ported by @The_Killer_Bob """
 
 import io
 from random import randint, uniform
